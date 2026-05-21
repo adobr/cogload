1a0799839adab644917c982db46ee5b9864dd74a
Alexandra Dobrokhotova <dobrokhotova@gmail.com>
2026-05-21 13:14:59 +0300

    fix fact

--- scripts/fact.R
+++ scripts/fact.R
@@ -1,36 +1,25 @@
-library(psych)
+library(lavaan)
 library(dplyr)
-library(tidyr)
 
 data <- readRDS('/Users/dobrokhotova/Documents/GitHub/cogload/data/data1.rds')
 
-
-leppink_vars <- c(
-  "IL1",
-  "IL2",
-  "IL3",
-  "EL1",
-  "EL2",
-  "EL3",
-  "GL1",
-  "GL2",
-  "GL3",
-  "GL4"
-)
-
-
-# Выбрать только нужные переменные
+leppink_vars <- c("IL1","IL2","IL3","EL1","EL2","EL3","GL1","GL2","GL3","GL4")
 leppink_data <- data %>% select(all_of(leppink_vars))
 
-# Запустить FA с Oblimin
-fa_result <- fa(leppink_data,
-                nfactors = 3,
-                rotate = "oblimin",
-                fm = "ml",
-                scores = "regression")
+# CFA модель по структуре Leppink et al. (2013)
+model <- '
+  IL =~ IL1 + IL2 + IL3
+  EL =~ EL1 + EL2 + EL3
+  GL =~ GL1 + GL2 + GL3 + GL4
+'
 
-fa_result
+fit <- cfa(model, data = leppink_data, estimator = "ML")
 
+# Основные результаты
+summary(fit, fit.measures = TRUE, standardized = TRUE)
 
-# Factor loadings
-fa_result$loadings %>% print(cutoff = 0.3)
+# Индексы подгонки отдельно
+fitMeasures(fit, c("cfi", "tli", "rmsea", "rmsea.ci.lower", "rmsea.ci.upper", "srmr"))
+
+# Факторные нагрузки
+standardizedSolution(fit) %>% filter(op == "=~")
